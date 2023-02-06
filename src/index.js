ff3e6105054f17aac42cffee95211d379f613593
Kateryna Tonapetian <106622532+Diverona@users.noreply.github.com>
2023-02-06 19:12:21 +0200

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arrOfDec = getArrOfDecades();
+
+  let arrOfCodedLetters = getArrOfCodedLetters(arrOfDec);
+
+  let resStr = turnMorseCodeToWords(arrOfCodedLetters);
+
+  return resStr;
+
+  function getArrOfDecades() {
+    let arr = [];
+    for (i = 0; i < expr.length; i += 10) {
+      arr.push(expr.slice(i, i + 10));
+    }
+    return arr;
+  }
+
+  function getArrOfCodedLetters(arr) {
+    let arrMorseCoded = [];
+    for (let el of arr) {
+      let currStr = "";
+      for (i = 0; i < el.length; i += 2) {
+        el[i] === "1" && el[i + 1] === "0"
+          ? (currStr += ".")
+          : el[i] === "1" && el[i + 1] === "1"
+          ? (currStr += "-")
+          : el[i] === "*"
+          ? (currStr += " ")
+          : (currStr += "");
+      }
+
+      arrMorseCoded.push(currStr);
+    }
+    return arrMorseCoded;
+  }
+
+  function turnMorseCodeToWords(arr) {
+    return arr
+      .map((l) => (MORSE_TABLE[l] ? (l = MORSE_TABLE[l]) : (l = " ")))
+      .join("");
+  }
 }
 
 module.exports = {
